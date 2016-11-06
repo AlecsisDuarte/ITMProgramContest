c0fc6a51499f8f8383fc35c9f5012dc5e1a2bffc
Alexis Duarte <alexis.duarte@hotmail.com>
2016-11-06 13:22:30 -0800

    updated words to spanish

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -47,8 +47,8 @@ $bundle = yiister\gentelella\assets\Asset::register($this);
                         <img src="http://placehold.it/128x128" alt="..." class="img-circle profile_img">
                     </div>
                     <div class="profile_info">
-                        <span>Welcome,</span>
-                        <h2>John Doe</h2>
+                        <span>Bienvenido,</span>
+                        <h2>Alexis Duarte</h2>
                     </div>
                 </div>
                 <!-- /menu prile quick info -->
@@ -165,22 +165,22 @@ $bundle = yiister\gentelella\assets\Asset::register($this);
                     <ul class="nav navbar-nav navbar-right">
                         <li class="">
                             <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                                <img src="http://placehold.it/128x128" alt="">John Doe
+                                <img src="http://placehold.it/128x128" alt="">Alexis Duarte
                                 <span class=" fa fa-angle-down"></span>
                             </a>
                             <ul class="dropdown-menu dropdown-usermenu pull-right">
-                                <li><a href="javascript:;">  Profile</a>
+                                <li><a href="javascript:;">  Perfil</a>
                                 </li>
                                 <li>
                                     <a href="javascript:;">
                                         <span class="badge bg-red pull-right">50%</span>
-                                        <span>Settings</span>
+                                        <span>Opciones</span>
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="javascript:;">Help</a>
+                                    <a href="javascript:;">Ayuda</a>
                                 </li>
-                                <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
+                                <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Cerrar Sesion</a>
                                 </li>
                             </ul>
                         </li>
